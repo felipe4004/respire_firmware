197d80a9ee9f6c48f1d19258fc506ec72559af04
felipe4004 <felipe4004@gmail.com>
2020-10-31 15:49:11 -0300

    integracao display e motor

---
+++ motor_display_ard/motor_display_ino/respire.h
@@ -0,0 +1,75 @@
+#ifndef _RESPIRE_H_
+#define _RESPIRE_H_
+
+#include <Arduino.h>
+#include <U8glib.h>
+
+
+/*-------------------MOTOR------------------*/
+
+#define PUL 9
+#define DIR 10
+//#define EN    //Nao sei o que significa esse pino 
+
+
+/*--------------------LCD-----------------*/
+//Configuração de Pinagem, Enable, RW, RS, RESET
+
+
+
+#define UPDATE_LCD_TIME 40
+
+
+//Declaracao das variaveis
+
+int p_plot[64];
+int f_plot[64];
+bool lcdFlag;
+char str[127];
+
+
+
+/*-----------------------MEDICOES*-------------*/
+/*
+ *  MEASUREMENTS
+ *  daq.adcValue  : Array com valores em unidades de conversão do fluxo do ar (0), tensão elétrica na bateria (1), 
+ *                  e pressão do ar (2) (0-2013) e temperatura interna do microcontrolador (3). 
+ *  daq.pres      : Valor de pressão do ar (+/-50.0 cmH2O). Ponto fixo, uma casa decimal.
+ *  daq.flow      : Valor de fluxo do ar (+/- 21.0 L/min ). Ponto fixo, uma casa decimal.
+ *  daq.vBat      : Tensão elétrica na bateria (0.0-14.0 V). Ponto fixo, uma casa decimal.
+ *  daq.temp      : Temperatura do ar (0.0-50.0 °C). Ponto fixo, uma casa decimal.
+ *  daq.volCorr   : Volume corrente de ar (0-700 mL). Inteiro
+ *  daq.volMinu   : Volume / minuto de ar (0-21000 mL/min). Inteiro
+ *  daq.freq      : Frequência respiratória medida em respiração asistida [0.0-30.0 Ciclos/min]. Ponto fixo, uma casa decimal.
+ *  daq.presPla   : Pressão de platô (+/-50.0 cmH2O). Ponto fixo, uma casa decimal.
+ *  
+ *  As medições obtidas do ADC (apenas os valores 0-2 do adcValue) devem ser convertidas a unidades de engenharia 
+ *  no formato definido e armazenadas na variável correspondente. 
+ *  As outras medições serão calculadas no módulo de processamento ou obtidas por outros métodos.
+ */
+
+struct daq_type {
+    int           pres;
+    int           flow;
+    byte          vBat;
+    int           temp;
+    int           volCorr;
+    unsigned int  volMinu;
+    int           freq;
+    int           presPla;
+} daq;
+
+
+
+
+
+/*------------PROTOTIPOS DE FUNCOES----------------*/
+
+void u8g_prepare(void);
+void u8g_frame(void);
+void u8g_string(void);
+void draw(void);
+void measureFunction(void);
+
+
+#endif
---
+++ motor_display_ard/src/main.cpp
@@ -0,0 +1,169 @@
+#include "respire.h"
+
+
+U8GLIB_ST7920_128X64_1X u8g(13, 11, A1);
+
+
+void setup(void) {
+  // Setup Timer 0
+  TCCR0A = bit(WGM01);                // CTC operation
+  TCCR0B = bit(CS01) | bit(CS00);     // pre-scaling 64
+  OCR0A =  250;                       //  time = 250 * 64 * 1/16000000) = 1 ms
+  TIMSK0 = bit (OCIE0A);              // interrupt on Compare A Match
+
+  // Setup Timer 1 & Motor
+  TCCR1A = 0;                         // CTC operation, toggle OC1A on compare match if motor on
+  TCCR1B = bit(WGM12) | bit(CS11) | bit(CS10);   // pre-scaling 64
+  OCR1A = 5;                      // time = 5*64*(1/16000000) = 20 us   
+  TIMSK1 = bit (OCIE1A);              // interrupt on Compare A Match
+
+
+  pinMode (PUL, OUTPUT);
+  pinMode (DIR, OUTPUT);
+  //pinMode (EN, OUTPUT);
+  //digitalWrite(EN,HIGH);
+
+
+  Serial.begin(9600);
+
+    // put your setup code here, to run once:
+}
+
+
+void loop(void) {
+  
+  u8g.firstPage(); //Manutencao da figura no display
+
+  if(lcdFlag){
+
+    measureFunction(); //Funcao para a medicao ADC
+
+    //Escrita no display
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  lcdFlag = false;
+  }
+
+}
+
+
+/* 
+*  Timer0 ISR
+*/
+ISR(TIMER0_COMPA_vect){
+  static unsigned int timeCount=0;
+  if (timeCount==UPDATE_LCD_TIME){
+    lcdFlag=true;
+    timeCount=0;
+  }
+  timeCount++;
+
+}
+
+/* 
+ *  Timer1 ISR
+ */
+ISR(TIMER1_COMPA_vect){
+  static unsigned int steps=0;
+  static unsigned int interval=0;
+  static int stateMotor = 0;
+
+  switch(stateMotor){
+    case(0):      //define a direcao
+      steps = 0;
+      interval = 0;
+      stateMotor = 1;
+      digitalWrite(DIR, LOW);
+    case(1):      //coloca o pulso em nivel alto e aguarda 20us
+      digitalWrite(PUL, HIGH);
+      stateMotor = 2;
+      break;
+    case(2):      //coloca em nivel baixo e verifica se ja chegaram nas 20000 contagens
+      digitalWrite(PUL,LOW);
+      steps++;
+      steps < 20000 ? stateMotor=1 : stateMotor = 3;
+      break;
+    case(3):      //intervalo de 10 ms
+      interval++;
+      interval < 250 ? stateMotor = 3 : stateMotor = 4;
+      break;
+    case(4):      //define a direcao
+      digitalWrite(DIR,HIGH);
+      steps = 0;
+      interval = 0;
+      stateMotor = 5;
+    case(5):      //coloca o pulso em nivel alto e aguarda 20us
+      digitalWrite(PUL, HIGH);
+      stateMotor = 6;
+      break;
+    case(6):      //coloca o pulso em nivel baixo e verifica a condicao
+      digitalWrite(PUL,LOW);
+      steps++;
+      steps < 20000 ? stateMotor=5 : stateMotor = 0; //retorna para o se ja for atingido o valor de 20000
+      break;
+      
+  }
+
+}
+
+void u8g_prepare(void) {
+  //size_t i;
+  u8g.setColorIndex(1);
+  u8g.setFont(u8g_font_6x10);
+  u8g.setFontRefHeightExtendedText();
+  u8g.setDefaultForegroundColor();
+  u8g.setFontPosTop();
+
+
+}
+
+void u8g_frame(void) {
+
+  u8g.drawLine(0, 11, 0, 11+52);
+  u8g.drawLine(0, 11+52, 126/2, 11+52);
+  u8g.drawLine(128/2+2,11, 128/2+2, 11+52);
+  u8g.drawLine(128/2+2, 11+52, 128, 11+52);
+}
+
+
+void u8g_string(void) {
+  uint8_t t;
+  sprintf(str, "cmH2O    L/min");
+  t=u8g.getStrWidth(str);
+  u8g.drawStr(63 - t/2,0, str);
+}
+
+
+
+void draw(void) {
+  int j=0;
+  u8g_prepare();
+  u8g_string();
+  for(j=0; j<63; j++){
+    u8g.drawLine(j, p_plot[j], j+1, p_plot[j+1]);
+    u8g.drawLine(65+j, f_plot[j], 65+j+1, f_plot[j+1]);
+  }
+  u8g_frame();
+}
+
+
+void measureFunction(void){
+    int i = 0;
+    daq.flow = (int) (64.0-(53.0*analogRead(A5)/750.0));
+    daq.pres = (int) (64.0-(53.0*analogRead(A7)/500.0));
+
+    // insercao de um novo valor no vetor
+
+    for(i=0; i < 63; i++){
+      p_plot[i] = p_plot[i+1];
+      f_plot[i] = f_plot[i+1];
+    }
+    p_plot[i] = daq.pres;
+    f_plot[i] = daq.flow;
+
+}
+
+
+
+
